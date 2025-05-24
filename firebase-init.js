32dacd2ebde01d35c55f2aaa5b27367d2608b1bb
Rafayel-8 <rhakobyan156@gmail.com>
2025-05-24 15:11:16 +0400

    Add initial website files

---
+++ firebase-init.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCu-GxF46Pa5pz03eJ5fwOdeC1id74AsJA",
+    authDomain: "mywebsite-b6912.firebaseapp.com",
+    projectId: "mywebsite-b6912",
+    storageBucket: "mywebsite-b6912.appspot.com",
+    messagingSenderId: "431907285830",
+    appId: "1:431907285830:web:a8aa08c9438ad752e49347"
+  };
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+  
